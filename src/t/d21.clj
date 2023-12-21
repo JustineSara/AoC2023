0a5849c96808d9df6eeca0b1ccbe74269f0826ec
Justine Sara Pujos <pujos.justine.s@gmail.com>
2023-12-21 11:30:00 +0100

    d21 part1

--- src/t/d21.clj
+++ src/t/d21.clj
@@ -4,19 +4,74 @@
    [clojure.string :as cljstr]
    [clojure.set :as cljset]))
 
-(def sample "")
+(def sample "...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........")
 
 (defn parse-input
   [input]
-  (cljstr/split-lines input)
+  (->>
+   (cljstr/split-lines input)
+   (map-indexed (fn [y l] (map-indexed (fn [x c] [x y c]) l) ))
+   (apply concat)
+   (filter (fn [[_ _ c]] (not= c \#)))
+   ((fn [all]
+      [(->>
+        all
+        (filter (fn [[_ _ c]] (= c \S)))
+        first
+        ((fn [[x y _]] [x y]))
+        )
+       (->>
+        all
+        (map (fn [[x y _]] [x y]))
+        set)]))
+   )
 )
 
 
+(defn one-step
+  [gardens pos-s]
+  (->>
+   (for [[x y] pos-s]
+     (->> [[(inc x) y] [(dec x) y] [x (inc y)] [x (dec y)]]
+          (filter (fn [[x y]] (contains? gardens [x y])))
+          set))
+   (apply cljset/union))
+)
+
+
+(defn walk
+  [pos-s gardens Nsteps]
+  (loop [n Nsteps
+         p-s pos-s]
+    (println "  " n " : " p-s)
+    (if (zero? n)
+      (count p-s)
+      (->>
+       p-s
+       (one-step gardens)
+       (recur (dec n)))
+      )
+    )
+  )
+
+
 (defn d21p1
-  [input]
-  (let [x (parse-input input)
+  [input Nsteps]
+  (let [[pos0 gardens] (parse-input input)
         ]
-     x
+    (println "start at " pos0)
+    ;; (one-step [pos0 pos0] gardens)
+    (walk [pos0] gardens Nsteps)
   ))
 
 (defn d21p2
@@ -33,8 +88,8 @@
   (newline)
   
   (println "part1")
-  (prn (d21p1 sample))
-;;  (prn (d21p1 (slurp "input/day21.txt")))
+  (prn (d21p1 sample 6))
+;;  (prn (d21p1 (slurp "input/day21.txt") 64))
   
 ;;  (newline)
 ;;  (println "part2")
